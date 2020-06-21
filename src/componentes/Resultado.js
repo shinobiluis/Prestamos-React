66c67cfeaebfa04300fe7119943d95bafdfb2f90
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-06-20 20:34:51 -0500

    Mostrando el resultado

--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Resultado = () => {
+const Resultado = ({total, plazo, cantidad}) => {
     return (
-        <h1>Desde resultado</h1>
+        <div className="u-full-width resultado">
+            <h2>Resumen</h2>
+            <p>La cantiadd solicitada es ${cantidad}</p>
+            <p>A pagar en un plazo de: {plazo} meses</p>
+            <p>Su pago mensual es de: ${ (total / plazo).toFixed(2) }</p>
+            <p>Total a pagar: ${(total).toFixed(2)}</p>
+        </div>
     );
 };
  
